2a637eaa7fb4aa3a6f742db6387de98ac0ddb981
1002089 <1002089@HS-119-09.pasd.palisadessd.org>
2018-09-05 10:34:40 -0400

    Initial Commit

---
+++ APCompSciA/src/Lincoln.java
@@ -0,0 +1,41 @@
+
+public 
+class 
+Lincoln 
+{
+
+	public 
+	static 
+	void 
+	main
+	(
+			String
+			[
+			 ] 
+					 args
+			) 
+	{
+
+					//floof
+		System.
+		out.
+		println
+		(
+				"A quote by Abraham Lincoln:"
+				)
+		;
+
+		System.
+		out.
+		println
+		(
+				"Whatever you are, be a good one."
+				)
+		;
+
+
+
+
+	}
+
+}
