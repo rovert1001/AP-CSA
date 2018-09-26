00f90789f451060344fa52da54ae29b29fde1c3d
rovert101 <rovert101@DESKTOP-09CTP7A>
2018-09-26 00:51:27 -0400

    Finished Paint.java

--- APCompSciA/src/Paint.java
+++ APCompSciA/src/Paint.java
@@ -2,52 +2,66 @@
  * Paint.java
  *
  * Code Description: Determine how much paint is needed to paint the walls of a
- * room given its length, width, height, and how many doors and windows are in
- * the room.
+ * room while outlining and using its length, width, height, and how many doors
+ * and windows are in the room and outputs the result.
  * 
  * @author Trevor Slabicki
- * @version 9-24-2018
+ * @version 9-26-2018
  * @contact 1002089@palisadessd.org
  */
 
 public class Paint
 {
-  /**
-   * main method - Determine how much paint is needed to paint the walls of a
-   * room given its length, width, height, and how many doors and windows are in
-   * the room.
-   *
-   * @param args
-   */
-
   // declaring constants
   private static final int iCOVERAGE_PER_GALLON = 350;
   private static final int iDOOR_SIZE = 20;
   private static final int iWINDOW_SIZE = 15;
 
-  // declares integers for dimensions and obstacles of room
-  public static int iLength, iWidth, iHeight, iWindows, iDoors;
+  // declares integers for dimensions of room in feet
+  public static int iLength, iWidth, iHeight;
 
-  // declares doubles for Total Square Feet and Gallons Needed
+  // declares integers for number of doors and windows in the room
+  public static int iWindows, iDoors;
+
+  // declares doubles for Total Square Feet and Gallons needed
   public static double dSquareFeet, dGallonsNeeded;
+
+  /**
+   * main method - Determine how much paint is needed to paint the walls of a
+   * room while outlining and using its length, width, height, and how many
+   * doors and windows are in the room and outputs the result.
+   *
+   * @param args
+   */
 
   public static void main(String[] args)
   {
 
-    iLength = 0;
-    iWidth = 0;
-    iHeight = 0;
-    iWindows = 0;
-    iDoors = 0;
+    // Giving values to dimensions of room and number of windows and doors
+    iLength = 5;
+    iWidth = 4;
+    iHeight = 3;
+    iWindows = 2;
+    iDoors = 1;
 
-    dSquareFeet = 2 * iHeight * (iLength + iWidth)
-        - iDoors * iDOOR_SIZE - iWindows * iWINDOW_SIZE;
+    /*
+     * Calculates square footage of walls in the room using the rooom's length,
+     * width, and height and subtracting the area taken up by doors and windows.
+     */
+    dSquareFeet = 2 * iHeight * (iLength + iWidth) - iDoors * iDOOR_SIZE
+        - iWindows * iWINDOW_SIZE;
 
+    // Figures out how many gallons of paint are needed to paint the room
     dGallonsNeeded = dSquareFeet / iCOVERAGE_PER_GALLON;
 
+    /*
+     * Outputs statement saying dimensions, number of doors, and number of
+     * windows of the room as well as gallons of paint needed to paint the room
+     */
     System.out
         .println("For a room with a length of " + iLength + " ft, width of "
-            + iWidth + " ft, and a height of " + iHeight + " ft, it would take "
+            + iWidth + " ft, a height of " + iHeight + " ft, and with "
+            + iWindows + " windows and " + iDoors + " doors, it would take "
             + dGallonsNeeded + " gallons to paint the room.");
   }
 }
