e6b665f2050f9e3508034d0d57427b58cf247b39
1002089 <1002089@HS-117-24.pasd.palisadessd.org>
2019-01-15 10:46:07 -0500

    oi

--- APCompSciA/src/CountingChar.java
+++ APCompSciA/src/CountingChar.java
@@ -1,61 +1,66 @@
 import java.util.Scanner;
 
-public class CountingChar
-{
+public class CountingChar {
 
-  private static String sPhrase; // a string of characters
-  private static int iCountBlank; // the number of blanks (spaces) in the phrase
-  private static int iLength; // the length of the phrase
-  private static char cChar; // an individual character in the string
-  private static Scanner kb = new Scanner(System.in);
+	// A string of characters
+	private static String sPhrase;
 
-  public static void main(String[] args)
-  {
+	// The number of blanks (spaces) in the phrase
+	private static int iCountBlank;
 
-    System.out.println();
-    System.out.println("Character Counter");
-    System.out.println();
+	// The length of the phrase
+	private static int iLength;
 
-    System.out.print("Enter a sentence or phrase: ");
-    sPhrase = kb.nextLine();
-    iLength = sPhrase.length();
+	// An individual character in the string
+	private static char cChar;
 
-    System.out.println();
-    System.out.println("Number of blank spaces: " + charCounter(' '));
-    System.out.println();
-  }
+	// Scanner to read in strings
+	private static Scanner kb = new Scanner(System.in);
 
-  private static int charCounter(char cChar)
-  {
+	public static void main(String[] args) {
 
-    // Amount of times the given character appears in cChar
-    int iCharCount = 0;
+		System.out.println();
+		System.out.println("Character Counter");
+		System.out.println();
 
-    // Character at position in charList; used w/ for loop
-    char cCharAt;
+		System.out.print("Enter a sentence or phrase: ");
+		sPhrase = kb.nextLine();
+		iLength = sPhrase.length();
 
-    // Array of chars in sPhrase
-    char[] charList = sPhrase.toCharArray();
+		System.out.println();
+		System.out.println("Number of blank spaces: " + charCounter(' '));
+		System.out.println();
+	}
 
-    for (cCharAt = 0; cCharAt < charList.length; cCharAt++)
-    {
+	private static int charCounter(char cChar) {
 
-      if (charList[cCharAt] == cChar)
-        iCharCount++;
+		// Amount of times the given character appears in cChar
+		int iCharCount = 0;
 
-    }
+		// Character at position in charList; used w/ for loop
+		char cCharAt;
 
-    /*
-     * for loop above is equivalent (ignoring formatting) to what is below
-        
-        for (char cCharAt : sPhrase.toCharArray()) 
-        {
-          if (cCharAt == cChar) 
-            iCharCount++;
-        }  */
-    
+		// Array of chars in sPhrase
+		char[] charList = sPhrase.toCharArray();
 
-    return iCharCount;
+		for (cCharAt = 0; cCharAt < charList.length; cCharAt++) {
 
-  }
+			if (charList[cCharAt] == cChar)
+				iCharCount++;
+
+		}
+
+		/*
+		 * for loop above is equivalent (ignoring formatting) to what is below
+		 * 
+		 * for (char cCharAt : sPhrase.toCharArray()) {
+		 * 
+		 * if (cCharAt == cChar) 
+		 * 	 iCharCount++;
+		 *  }
+		 */
+
+		return iCharCount;
+
+	}
 }
---
+++ APCompSciA/src/SumEvens.java
@@ -0,0 +1,9 @@
+
+public class SumEvens {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
