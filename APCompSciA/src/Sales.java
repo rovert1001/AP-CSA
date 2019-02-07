79b1d368121f32c9e1b381735f92d4d403f3a976
rovert1001 <1002089@palisadessd.net>
2019-02-07 10:48:32 -0500

    saily boi

---
+++ APCompSciA/src/Sales.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+public class Sales
+{
+  public static void main(String[] args)
+  {
+    final int iSALESPEOPLE = 5;
+
+    int[] iSales = new int[iSALESPEOPLE];
+    int iSum;
+    int iIndex;
+
+    Scanner kb = new Scanner(System.in);
+
+    for (iIndex = 0; iIndex < iSales.length; iIndex++)
+    {
+      System.out.print("Enter sales for salesperson " + iIndex + ": $");
+      iSales[iIndex] = kb.nextInt();
+    }
+
+    System.out.println("\n  Sales Report");
+    System.out.println(" ------------------ ");
+
+    iSum = 0;
+    for (iIndex = 0; iIndex < iSales.length; iIndex++)
+    {
+      System.out.println(" " + iIndex + " $" + iSales[iIndex]);
+      iSum += iSales[iIndex];
+    }
+
+    System.out.println("\nTotal sales: $" + iSum);
+
+    kb.close();
+  }
+}
